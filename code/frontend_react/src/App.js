7a1cd2c914c089aedac71a68765f548b7738d106
SaiNavyanthv2000 <sainavyanthv@gmail.com>
2022-04-10 02:06:36 +0530

    updated about page

--- code/frontend_react/src/App.js
+++ code/frontend_react/src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import PredictPrice from "./components/PredictPrice";
-import ButtonAppBar from "./components/AppBar";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import PredictPrice from './components/PredictPrice';
+import ButtonAppBar from './components/AppBar';
+import About from './components/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/predict" element={<PredictPrice />}></Route>
+          <Route path="/about" element={<About />}></Route>
         </Routes>
       </Router>
     </>
